29aa0dee81d81a9b8c5a60c8436f06ebce085dfa
Artyom Yudin <yudin.artyom@gmail.com>
2021-04-12 17:45:17 +0300

    add init https server

---
+++ backend/src/modules/https-server.ts
@@ -0,0 +1,50 @@
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
+
+const hostname = '172.21.220.13';
+const port = 4443;
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+  // 'Access-Control-Max-Age': 2592000, // 30 days
+};
+
+function readCert() {
+  const readFile = util.promisify(fs.readFile);
+  let key;
+  let cert;
+
+  readFile(path.resolve(__dirname, '../cert/center_inform.key')).then(keyPem => {
+    key = keyPem;
+  });
+  readFile(path.resolve(__dirname, '../cert/center_inform.crt')).then(certPem => {
+    cert = certPem;
+  });
+  return [key, cert];
+}
+
+export function initHTTPSServer(): https.Server {
+  function onError(error: any) {
+    if (error.syscall !== 'listen') {
+      // throw error;
+      console.log("Well, this didn't work...");
+    }
+  }
+  const [key, cert] = readCert();
+  const server = https
+    .createServer({ key, cert }, (req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello World');
+    })
+    .listen(port, hostname, () => {
+      console.log(`Server is listening on port ${port}`);
+    })
+    .on('error', onError);
+  return server;
+}
