dabd90a3e3decef71af669c462b49ce5ffc9b23c
ArtyomYudin <yudin.artyom@gmail.com>
2022-08-31 16:38:41 +0300

    daily commit 31082022

--- frontend/src/app/shared/pipes/purchasetarget.pipe.ts
+++ frontend/src/app/shared/pipes/purchasetarget.pipe.ts
@@ -5,12 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PurchaseTargetPipe implements PipeTransform {
   public transform(value: any): any {
-    let itemString = '';
     // console.log(value.length);
     // return value.length > 1 ? value[0].item : `${value[0].item}и еще`;
-    value.forEach((itemValue: any) => {
-      itemString = `${itemString}, ${itemValue.item}`;
-    });
-    return itemString;
+    return value.length === 1 ? value[0].item : `${value[0].item} + ${value.length - 1} позиций`;
   }
 }
