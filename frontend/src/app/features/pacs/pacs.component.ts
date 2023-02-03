858332fe6dec34510ef5fdfdb2383f6deb26af4a
Artyom Yudin <yudin.artyom@gmail.com>
2023-02-03 20:02:00 +0300

    fixup

--- frontend/src/app/features/pacs/pacs.component.ts
+++ frontend/src/app/features/pacs/pacs.component.ts
@@ -4,6 +4,9 @@ import { Subject } from 'rxjs/internal/Subject';
 import { SubscriptionLike } from 'rxjs/internal/types';
 import { WebsocketService } from '@service/websocket.service';
 import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { ClarityModule } from '@clr/angular';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { Event } from '@service/websocket.service.event';
 import { DepartmentComponent } from './department/department.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -12,7 +15,7 @@ import { GuestComponent } from './guest/guest.component';
 @Component({
   selector: 'fe-pacs',
   standalone: true,
-  imports: [GuestComponent, EmployeeComponent, DepartmentComponent],
+  imports: [ClarityModule, ReactiveFormsModule, MatAutocompleteModule, GuestComponent, EmployeeComponent, DepartmentComponent],
   templateUrl: './pacs.component.html',
   styleUrls: ['./pacs.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -24,15 +27,20 @@ export default class PacsComponent implements OnInit {
 
   public departmentStructure: any[];
 
+  public employeeSearch: FormGroup;
+
   private ngUnsubscribe$: Subject<any> = new Subject();
 
-  constructor(private wsService: WebsocketService) {
+  constructor(private formBuilder: FormBuilder, private wsService: WebsocketService) {
     this.departmentStructureArray$ = this.wsService
       .on<any>(Event.EV_DEPARTMENT_STRUCTURE_BY_UPN)
       .pipe(distinctUntilChanged(), takeUntil(this.ngUnsubscribe$));
   }
 
   ngOnInit(): void {
+    this.employeeSearch = this.formBuilder.group({
+      employeeName: '',
+    });
     this.wsService.send('getPacsInitValue');
     this.wsService.send(
       'getDepartmentStructureByUPN',
